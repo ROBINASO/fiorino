63985a5c09b1533cc25962db83060bb94fc1b6a7
ROBINASO <luzzi.roberto@gmail.com>
2023-11-07 22:19:47 +0100

    Risoluzione Conflitti

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+import InputBalance from "./components/InputBalance";
+
+//Pagina iniziale funzione
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FIORINO</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//Stili
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/InputBalance.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Stylesheet, View } from "react-native";
+
+function InputBalance() {
+  return <View></View>;
+}
+
+export default InputBalance;
